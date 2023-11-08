46ca2db01f9a7fe282cc933ee5bfa5368a855283
Maxime <Maxime@maxime.ru>
2023-11-08 07:12:47 +0300

    Домашняя работа 16

--- src/edu/itmo/project/lesson1516/Fruit.java
+++ src/edu/itmo/project/lesson1516/Fruit.java
@@ -1,11 +1,15 @@
 package edu.itmo.project.lesson1516;
 
+import java.util.Objects;
+
 public class Fruit {
     private final FruitType type;
     private double price;
     private int count;
 
     public Fruit(FruitType type, double price, int count) {
+        Objects.requireNonNull(type);
+        if (price <= 0 || count <= 0) throw new IllegalArgumentException("цена и количество должны быть больше 0");
         this.type = type;
         this.price = price;
         this.count = count;
@@ -31,12 +35,30 @@ public class Fruit {
         this.count = count;
     }
 
-
     public boolean equals(Fruit fruit) {
         if (this.type == fruit.getType() && this.price == fruit.getPrice()) return true;
         return false;
     }
 
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = type.hashCode();
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + count;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "type=" + type +
+                ", price=" + price +
+                ", count=" + count +
+                '}';
+    }
 
     public enum FruitType {
         APPLE, PEAR, BANANA, APRICOT
--- src/edu/itmo/project/lesson1516/FruitStorage.java
+++ src/edu/itmo/project/lesson1516/FruitStorage.java
@@ -1,17 +1,21 @@
 package edu.itmo.project.lesson1516;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class FruitStorage {
     private int numberOfSlots;
-    private ArrayList<Fruit> fruits;
+    private ArrayList<Fruit> fruits = new ArrayList<>();
 
     public FruitStorage(int numberOfSlots) {
         this.numberOfSlots = numberOfSlots;
     }
 
+    public int getNumberOfSlots() {
+        return numberOfSlots;
+    }
+
     public boolean addToStorage(Fruit fruit) {
-        if (fruit == null || fruits.contains(fruit) || fruits.size() == numberOfSlots) {
+        if (fruit == null /*|| fruits.contains(fruit)*/) {
             return false;
         } else {
             for (int i = 0; i < fruits.size(); i++) {
@@ -28,5 +32,62 @@ public class FruitStorage {
             numberOfSlots -= fruit.getCount();
             return true;
         }
+    }
+
+    // вернуть количество фруктов определённого типа
+    public int getNumberOfFruitsByType(Fruit.FruitType fruitType) {
+        int numberOfFruits = 0;
+        for (Fruit fruit:fruits) {
+            if (fruit != null && fruitType != null && fruit.getType().equals(fruitType)) {
+                numberOfFruits += fruit.getCount();
+            }
+        }
+        return numberOfFruits;
+    }
+
+    // вернуть количество свободных мест в хранилище
+    public int getNumberOfEmptySlots(){
+        return this.numberOfSlots;
+    }
+
+    public void increasePrice(int value){
+        // value может быть в диапазоне [10; 30)
+        // увеличить цену всех фруктов на value процентов
+        if (!(value >= 10 && value < 30)) throw new IllegalArgumentException("value может быть в диапазоне [10; 30)");
+        for (Fruit fruit:fruits) {
+            fruit.setPrice(fruit.getPrice() * (1d + (double)value/100));
+        }
+    }
+
+    public List<Fruit> getFruitsByTypeAndPrice(Fruit.FruitType fruitType, int maxPrice) {
+        List<Fruit> fruitList = new ArrayList<>();
+        Objects.requireNonNull(fruitType);
+        if (maxPrice <= 0) throw new IllegalArgumentException("maxPrice должна быть положительной");
+        // maxPrice должна быть положительной, fruitType не null
+        // возвращает список, в который войдут фрукты из коллекции fruits
+        // с типом fruitType и ценой не выше maxPrice
+        for (Fruit fruit:fruits) {
+            if (fruit.getType().equals(fruitType) && fruit.getPrice() < maxPrice) fruitList.add(fruit);
+        }
+        return fruitList;
+    }
+
+    public Set<Fruit.FruitType> getFruitTypes(){
+        Set<Fruit.FruitType> fruitTypeSet = new HashSet<>();
+        for (Fruit fruit:fruits) {
+            fruitTypeSet.add(fruit.getType());
+        }
+        return fruitTypeSet;
+    }
+
+    public double getMinPriceByType(Fruit.FruitType fruitType){
+        Objects.requireNonNull(fruitType);
+        double minPrice = 0;
+        for (Fruit fruit:fruits) {
+            if (fruit.getType().equals(fruitType) && (fruit.getPrice() < minPrice || minPrice == 0)) {
+                minPrice = fruit.getPrice();
+            }
+        }
+        return minPrice;
     }
 }
